86b5d779a20a587a68db3373de75ca89541b54b5
pinchen <pcchou1997@gmail.com>
2023-01-15 04:34:55 +0800

    finish the function of rendering all msg on msg board

--- public/index.js
+++ public/index.js
@@ -3,13 +3,34 @@ const imageInput = document.querySelector("#file-uploader");
 const comment = document.querySelector("#comment");
 const msgBoard = document.querySelector(".msgboard");
 
+fetch("/read")
+  .then((res) => {
+    return res.json();
+  })
+  .then((jsonResponse) => {
+    console.log(jsonResponse);
+    for (let i = jsonResponse.length - 1; i >= 0; i--) {
+      let newNode = document.createElement("div");
+      newNode.setAttribute("class", "message");
+      let content = `
+        <div class="message_comment">${jsonResponse[i].comment}</div>
+        <img class="message_image" src="${jsonResponse[i].image}" alt="">`;
+      newNode.innerHTML = content;
+      msgBoard.appendChild(newNode);
+    }
+  })
+  .catch((err) => {
+    // handle error
+    console.error(err);
+  });
+
 button.addEventListener("click", async (event) => {
   event.preventDefault();
   const file = imageInput.files[0];
-  console.log("imageInput:", imageInput);
   console.log("file:", file);
   const commentValue = comment.value;
-  console.log("commentValue:", commentValue);
+  imageInput.value = "";
+  comment.value = "";
 
   if (file == undefined || commentValue == "") {
     alert("任一欄位不可空白");
@@ -44,12 +65,24 @@ button.addEventListener("click", async (event) => {
 
     // read RDS
 
+    while (msgBoard.hasChildNodes()) {
+      msgBoard.removeChild(msgBoard.firstChild);
+    }
     await fetch("/read")
       .then((res) => {
         return res.json();
       })
       .then((jsonResponse) => {
         console.log(jsonResponse);
+        for (let i = jsonResponse.length - 1; i >= 0; i--) {
+          let newNode = document.createElement("div");
+          newNode.setAttribute("class", "message");
+          let content = `
+        <div class="message_comment">${jsonResponse[i].comment}</div>
+        <img class="message_image" src="${jsonResponse[i].image}" alt="">`;
+          newNode.innerHTML = content;
+          msgBoard.appendChild(newNode);
+        }
       })
       .catch((err) => {
         // handle error
